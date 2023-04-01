89daf7be58c901e9dae9d61fd378d298035bcb03
Kornilovich Mikhail <kornilovichmihail@gmail.com>
2023-04-01 15:19:52 +0300

    fixed

--- calculator.h
+++ calculator.h
@@ -4,6 +4,38 @@
 namespace linalg {
     class Calculator {
     public:
-        static std::vector<std::string>  solveSystemOfEquations(const Matrix& A, const Matrix& B);
+        static std::vector<std::string>  solveSystemOfEquations(const Matrix& A, const Matrix& B) {
+            if (A.getN() != B.getN()) {
+                throw std::invalid_argument("Count of rows of the matrix B must be equal to count of rows of the matrix A");
+            } else if (B.getM() != 1) {
+                throw std::invalid_argument("Count of columns of the matrix B must be equals 1");
+            }
+            Matrix C(A.getN(), A.getM() + B.getM());
+            for (std::size_t i = 0; i < A.getN(); i++) {
+                for (std::size_t j = 0; j < A.getM(); j++) {
+                    C[i][j] = A[i][j];
+                }
+            }
+            for (std::size_t i = 0; i < A.getN(); i++) {
+                C[i][A.getM()] = B[i][0];
+            }
+            C = C.triangle();
+
+            std::vector<std::string> ans;
+            for (int i = C.getN() - 1; i >= 0; i--) {
+                std::string str;
+                for (std::size_t j = 0; j < C.getM() - 1; j++) {
+                    if (C[i][j] != 0) {
+                        str += std::to_string(C[i][j]) + " * x" + std::to_string(j + 1) + " +";
+                    }
+                }
+                if (!str.empty()) {
+                    str.pop_back();
+                    str += "= " + std::to_string(C[i][C.getM() - 1]);
+                    ans.push_back(str);
+                }
+            }
+            return ans;
+        }
     };
 }
