6f7f159d6d16799f440b2c5e86d37740df37b289
Kornilovich Mikhail <kornilovichmihail@gmail.com>
2023-04-18 21:27:48 +0300

    added try-catch

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        final Scanner scanner = new Scanner(System.in);
-        Parser parser = new Parser();
-        System.out.println(parser.parse(scanner.nextLine()));
+        try {
+            final Scanner scanner = new Scanner(System.in);
+            Parser parser = new Parser();
+            System.out.println(parser.parse(scanner.nextLine()));
+
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
