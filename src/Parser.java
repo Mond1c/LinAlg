66eec10c840a49304a4fbeb7109af097fe28d2f3
Kornilovich Mikhail <kornilovichmihail@gmail.com>
2023-04-19 10:13:36 +0300

    added exceptions to parser

--- src/Parser.java
+++ src/Parser.java
@@ -49,6 +49,18 @@ public class Parser {
     }
 
     private Matrix parseMatrix(String str) {
+        str = str.trim();
+        int bracketCount = 0;
+        for (char ch : str.toCharArray()) {
+            if (ch == '{') {
+                bracketCount++;
+            } else {
+                break;
+            }
+        }
+        if (bracketCount > 2) {
+            throw new IllegalArgumentException("It's not a matrix! " + str);
+        }
         int n = Math.max(1, str.length() - str.replace("{", "").length() - 1);
         int m = 0;
         for (int i = 2; i < str.length(); i++) {
@@ -76,6 +88,9 @@ public class Parser {
     }
 
     private String solve() {
+        if (parts.size() != 3) {
+            throw new IllegalArgumentException("To solve system you need to write: A = B where A and B are matrices");
+        }
         Matrix a = parseMatrix(parts.get(0));
         Matrix b = parseMatrix(parts.get(2));
         return Calculator.solveSystemOfEquations(a, b);
@@ -91,6 +106,9 @@ public class Parser {
     }
 
     private double scalarCalculation() {
+        if (parts.size() != 3) {
+            throw new IllegalArgumentException("To make calculation with scalars you need to write: a +/-/*// b where a and b are scalars");
+        }
         double a = Double.parseDouble(parts.get(0));
         double b = Double.parseDouble(parts.get(2));
         return switch (parts.get(1).charAt(0)) {
@@ -103,6 +121,9 @@ public class Parser {
     }
 
     private String matrixCalculation() {
+        if (parts.size() != 3) {
+            throw new IllegalArgumentException("To make calculation with matrices you need to write: A +/-/* B where A and B are matrices");
+        }
         boolean aIsMatrix = parts.get(0).contains("{");
         boolean bIsMatrix = parts.get(2).contains("{");
         if (aIsMatrix && bIsMatrix) {
@@ -129,10 +150,16 @@ public class Parser {
     }
 
     private String findDeterminant() {
+        if (parts.size() != 1) {
+            throw new IllegalArgumentException("To find determinant of a matrix you need to write: det A where A is the matrix");
+        }
         return String.valueOf(parseMatrix(parts.get(0)).determinant());
     }
 
     private String inverse() {
+        if (parts.size() != 1) {
+            throw new IllegalArgumentException("To inverse a matrix you need to write: inverse A where A is the matrix");
+        }
         Matrix a = parseMatrix(parts.get(0));
         a.inverse();
         ;
@@ -140,17 +167,26 @@ public class Parser {
     }
 
     private String transpose() {
+        if (parts.size() != 1) {
+            throw new IllegalArgumentException("To transpose a matrix you need to write: transpose A where A is the matrix");
+        }
         Matrix a = parseMatrix(parts.get(0));
         a.transpose();
         return a.toString();
     }
 
     private String rank() {
+        if (parts.size() != 1) {
+            throw new IllegalArgumentException("To find rank of a matrix you need to write: rank A where A is the matrix");
+        }
         Matrix a = parseMatrix(parts.get(0));
         return String.valueOf(a.rank());
     }
 
     private String triangle() {
+        if (parts.size() != 1) {
+            throw new IllegalArgumentException("To triangle a matrix you need to write: triangle A where A is the matrix");
+        }
         Matrix a = parseMatrix(parts.get(0));
         a.triangle();
         return a.toString();
