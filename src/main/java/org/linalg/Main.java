f646573b274b7f58e891bfccac3082ed60360eed
Kornilovich Mikhail <kornilovichmihail@gmail.com>
2023-05-31 14:35:05 +0300

    added calculation

--- src/main/java/org/linalg/Main.java
+++ src/main/java/org/linalg/Main.java
@@ -11,7 +11,7 @@ public class Main {
     public static void main(String[] args) {
         try {
             final Scanner scanner = new Scanner(System.in);
-            System.out.println(new Parser().parse(scanner.nextLine()).evaluate().toLatexString());
+            System.out.println(new Parser().parse(scanner.nextLine()).evaluate(BigDecimal.ZERO).toLatexString());
 
         } catch (RuntimeException e) {
             System.out.println(e.getMessage());
