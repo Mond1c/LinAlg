1e57807e96c80c21794ac3f17ff2e7c9029c64bd
Kornilovich Mikhail <kornilovichmihail@gmail.com>
2023-06-02 16:43:32 +0300

    added variable indicate by user

--- src/main/java/org/linalg/Main.java
+++ src/main/java/org/linalg/Main.java
@@ -11,8 +11,24 @@ public class Main {
     public static void main(String[] args) {
         try {
             final Scanner scanner = new Scanner(System.in);
-            System.out.println(new Parser().parse(scanner.nextLine()).evaluate(BigDecimal.ZERO).toLatexString());
-
+            String request = scanner.nextLine();
+            if (request.contains(" at ")) {
+                String[] parts = request.split("at");
+                if (!parts[1].contains("=")) {
+                    throw new RuntimeException("You need to use =");
+                }
+                String[] variables = parts[1].split("=");
+                if (variables.length != 2) {
+                    throw new RuntimeException("You can use only one variable, example: at x = 3");
+                }
+                if (!variables[0].equals("x")) {
+                    throw new RuntimeException("You can use only x variable!");
+                }
+                BigDecimal x = new BigDecimal(variables[1]);
+                System.out.println(new Parser().parse(parts[0]).evaluate(x).toLatexString());
+            } else {
+                System.out.println(new Parser().parse(request).evaluate().toLatexString());
+            }
         } catch (RuntimeException e) {
             System.out.println(e.getMessage());
         }
