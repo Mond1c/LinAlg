adf52c59b187e8951078a6a80c6bfdb34a0f9c8a
Mikhail Kornilovich <kornilovichmihail@gmail.com>
2023-07-01 09:14:25 +0300

    simplify updated

--- src/test/java/TestParser.java
+++ src/test/java/TestParser.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class TestParser {
     private final static Parser PARSER = new Parser();
@@ -13,7 +14,7 @@ public class TestParser {
 
     @Test
     public void testConstExpression() {
-        Assertions.assertEquals(new Const(BigDecimal.valueOf(4.8)), PARSER.parse("1 + 2 + 3 / 5 * (1 + 2)").evaluate(BigDecimal.ZERO));
+        Assertions.assertEquals(new Const(BigDecimal.valueOf(4.8).divide(BigDecimal.ONE, 10, RoundingMode.HALF_UP)), PARSER.parse("1 + 2 + 3 / 5 * (1 + 2)").evaluate(BigDecimal.ZERO));
 
     }
 
